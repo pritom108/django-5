c38e7f96c2e4bccfdf03687ad8ff1bcf0c0b979a
Pritom Karmoker <karmokerpritom08@gmail.com>
2026-02-15 22:16:31 +0600

    ch21 added

--- ch21/core/views.py
+++ ch21/core/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-
 def home(req):
-    return render(req, 'core/home.html')
-
+    return render(req, 'core/home.html',{'list_element': 'Python'})
 
 def about(req):
     return render(req, 'core/about.html')
